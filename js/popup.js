e66cc5442223a764d5d670e878360ea1f24b37f5
Drafter Furniture <reddamaskus@gmail.com>
2026-03-09 09:08:17 +0700

    Create popup.js

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+const popup = document.getElementById("popup")
+
+function openPopup(html){
+
+popup.innerHTML = `<div class="popup-box">${html}</div>`
+
+popup.style.display="flex"
+
+}
+
+popup.onclick = e=>{
+
+if(e.target===popup) popup.style.display="none"
+
+}
